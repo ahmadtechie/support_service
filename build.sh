a4da28adf07f533468e10a2335e969784fc99e12
ahmadtechie <119786006+ahmadtechie@users.noreply.github.com>
2023-11-15 11:06:25 +0100

    modified build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 set -o errexit  # exit on error
 
-pip install -r requirements.txt
-
+pip install --upgrade pip && pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
